2fe08c7cfdffeaf7ab461e3650fedd53a79307fc
piscoyabenitescarlosandre-netizen <piscoyabenitescarlosandre@gmail.com>
2026-05-26 21:05:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,8 @@ st.title ("PROYECTO 1 – APLICACIÓN EN STREAMLIT")
 
 st.sidebar.title("Selecciona")
 
+st.write("Elaborado por: Carlos Piscoya Benites")
+st.write("Módulo 1 – Python Fundamentals")
+
+
 Selecciona = st.sidebar.selectbox("Menú", ["Home", "Ejercicio 1", "Ejercicio 2", "Ejercicio 3", "Ejercicio 4"])
