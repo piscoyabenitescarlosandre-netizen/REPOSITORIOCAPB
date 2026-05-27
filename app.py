b0c38ab7a1713770990323bbc4223aa79365079e
piscoyabenitescarlosandre-netizen <piscoyabenitescarlosandre@gmail.com>
2026-05-26 20:56:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,14 @@ import streamlit as st
 st.title ("PROYECTO 1 – APLICACIÓN EN STREAMLIT")
 
 st.sidebar.title("Selecciona")
+
 st.sidebar.selectbox("Home")
+
 st.sidebar.selectbox("Ejercicio 1")
+
 st.sidebar.selectbox("Ejercicio 2")
+
 st.sidebar.selectbox("Ejercicio 3")
+
 st.sidebar.selectbox("Ejercicio 4)
+                     
