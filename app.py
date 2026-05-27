4278e25baad77e4e273485a60485dbc8788b1c10
piscoyabenitescarlosandre-netizen <piscoyabenitescarlosandre@gmail.com>
2026-05-26 22:07:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,10 @@ st.write("Módulo 1 – Python Fundamentals")
 
 Selecciona = st.sidebar.selectbox("Menú", ["Home", "Ejercicio 1", "Ejercicio 2", "Ejercicio 3", "Ejercicio 4"])
 
-if Selecciona == "Ejercicio 1":
+if Selecciona == "Home":
+    st.write("Bienvenido a Home")
+
+elif Selecciona == "Ejercicio 1":
     st.write("Bienvenido al ejercicio 1")
 
 elif Selecciona == "Ejercicio 2":
