0c8b7c9fa6ff0a825962137c34d6ec2deab84305
piscoyabenitescarlosandre-netizen <piscoyabenitescarlosandre@gmail.com>
2026-05-26 21:55:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,3 +9,30 @@ st.write("Módulo 1 – Python Fundamentals")
 
 
 Selecciona = st.sidebar.selectbox("Menú", ["Home", "Ejercicio 1", "Ejercicio 2", "Ejercicio 3", "Ejercicio 4"])
+
+if menu == "Home":
+    st.title("Mi Proyecto en Streamlit")
+    st.write("Nombre: Carlos Piscoya")
+    st.write("Módulo: Python Fundamentals")
+    st.write("Descripción: Aplicación con ejercicios básicos")
+
+if menu == "Ejercicio 1":
+
+    if "movimientos" not in st.session_state:
+        st.session_state.movimientos = []
+
+    concepto = st.text_input("Concepto")
+    tipo = st.selectbox("Tipo", ["Ingreso", "Gasto"])
+    valor = st.number_input("Valor")
+
+    if st.button("Agregar"):
+        st.session_state.movimientos.append((concepto, tipo, valor))
+
+    st.write(st.session_state.movimientos)
+
+    ingresos = sum(v for c,t,v in st.session_state.movimientos if t=="Ingreso")
+    gastos = sum(v for c,t,v in st.session_state.movimientos if t=="Gasto")
+
+    st.write("Ingresos:", ingresos)
+    st.write("Gastos:", gastos)
+    st.write("Saldo:", ingresos - gastos)
